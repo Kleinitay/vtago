49d08031315008869de8751791fe9864a26a1636
kleinitay <itay@ubuntu.(none)>
2012-03-04 15:48:27 +0200

    changed facebook details

--- config/environment.rb
+++ config/environment.rb
@@ -13,8 +13,8 @@ end
 
 # for Facebook Connect
 FB_APP_KEY = Facebook::APP_ID
-FB_APP_SECRET = "2eab4df3fb3f1397d6f6ddca929db4af"
+FB_APP_SECRET = "379c4af3ea5265646256b5fcc0cde637"
 FB_APP_ID = Facebook::APP_ID
-FB_SITE_URL = "localhost:3000/" #temp => http:www.vtago.com
+FB_SITE_URL = "http://vtago.heroku.com/" #temp => http:www.vtago.com
 
 
