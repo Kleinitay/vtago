93a1e537a0230fbda42b7683098cc1c1f30b3b7f
Boris Dinkevich <Boris@dinkevich.com>
2012-03-21 15:42:15 +0200

    Move S3 variables to config/s3.yml

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,6 +1,21 @@
+module Amazon
+  config_path = File.join(Rails.root, "config", "s3.yml")
+  CONFIG = YAML.load_file(config_path)[Rails.env]
+  KEY    = CONFIG['key']
+  SECRET = CONFIG['secret']
+  BUCKET = CONFIG['bucket']
+end
+
 CarrierWave.configure do |config|
   config.permissions = 0777
-      config.s3_access_key_id = 'AKIAJPJLRRU2UE2NWMYQ'
-      config.s3_secret_access_key = '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'
-      config.s3_bucket = 'VideoStorageStaging'
+  config.s3_access_key_id = Amazon::KEY
+  config.s3_secret_access_key = Amazon::SECRET
+  config.s3_bucket = Amazon::BUCKET
+
+  config.fog_credentials = {
+    :provider               => 'AWS',
+    :aws_access_key_id      => Amazon::KEY,
+    :aws_secret_access_key  => Amazon::SECRET,
+  }
+  config.fog_directory  = Amazon::BUCKET
 end
