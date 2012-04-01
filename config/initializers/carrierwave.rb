9bbd624f42b2dd7c2677f552c3c822f026f3940e
Boris Dinkevich <Boris@dinkevich.com>
2012-04-01 13:25:53 +0300

    Added Amazon.yml with S3 details

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,6 +1,15 @@
+module Amazon
+  config_path = File.join(Rails.root, "config", "amazon.yml")
+  CONFIG      = YAML.load_file(config_path)[Rails.env]
+  KEY         = CONFIG['key']
+  SECRET      = CONFIG['secret']
+  BUCKET      = CONFIG['bucket']
+  FOLDER      = CONFIG['folder']
+end
+
 CarrierWave.configure do |config|
   config.permissions = 0777
-      config.s3_access_key_id = 'AKIAJLFBBEDDLZFLJ4DA'
-      config.s3_secret_access_key = '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'
-      config.s3_bucket = 'vtago_videos'
+  config.s3_access_key_id = Amazon::KEY
+  config.s3_secret_access_key = Amazon::SECRET
+  config.s3_bucket = Amazon::BUCKET
 end
