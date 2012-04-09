029ed21f3bafcb3c04693cc2db08871ba4c9b3e5
Boris Dinkevich <Boris@dinkevich.com>
2012-04-09 15:58:08 +0300

    Make prettier notifications

--- public/javascripts/notifications.js
+++ public/javascripts/notifications.js
@@ -1,29 +1,64 @@
-function check_notifications() {
-  $.getJSON('/notifications/count.json', function(data) {
-    var counter = $('#notifications_count');
+var notifications = function() { 
+  return { 
+    check: function() {
+      $.getJSON('/notifications/count.json', function(data) {
+        var counter = $('#notifications_count');
 
-    if (data > 0) {
-      counter.html(data);
-      counter.show();
-    } else {
-      counter.hide();
-    }
-  });
-}
+        if (data > 0) {
+          counter.html(data).show();
+        } else {
+          counter.hide();
+        }
+      });
+    },
 
-function show_notifications() {
-  console.log('SHOW !');
-  $.get('/notifications/all', function(data) {
-    var elem = $('#notifications');
-    elem.html(data);
-    elem.show();
-  });
-}
+    hide: function() {
+      $('#notifications').hide();
+    },
+
+    show: function() { 
+      $.get('/notifications/all', function(data) {
+        var elem = $('#notifications');
+        elem.html(data);
+        elem.show();
+      });
+    },
+
+    toggle: function(event) {
+      if ($('#notifications').is(':visible')) {
+        notifications.hide();
+      } else {
+        notifications.show();
+      }
+      event.stopPropagation();
+    },
+
+    init: function() {
+      $('#notifications_count').click(notifications.toggle);
+
+      // Catch clicks outside notifications bar
+      $('html').click(function() {
+        notifications.hide();
+      });
+
+      // Catch clicks on notifications bar
+      $('#notifications').click(function(event){
+        event.stopPropagation();
+      });
+
+      // Catch ESC 
+      $(document).keyup(function(e) {
+        if (e.keyCode == 27) { notifications.hide(); }
+      });
+
+      //setInterval(notifications.check, 5000);
+      notifications.check();
+    },
+  }
+}();
 
 $(document).ready(function(){			
   if ($('#notifications_count').length > 0) {
-    setInterval(check_notifications, 50000);
-    check_notifications();
+    notifications.init();
   }
-  $('#notifications_count').live('click', show_notifications);
 });
