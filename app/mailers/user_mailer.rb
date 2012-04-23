49c8d29c8370f356329a63c5c84772e2c7617bf5
Elinor <elinor.dreamer@gmail.com>
2012-04-23 13:16:01 +0300

    Adding methods for landing page email subscribe + thank you page + fixing emails delivery

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,8 +1,12 @@
- class UserMailer < ActionMailer::Base
-   default :from => "subscribe@vtago.com"
+class UserMailer < ActionMailer::Base
+  default :from => "subscribe@vtago.com"
 
-   def registration_confirmation(user)
-     @user = user
-     mail(:to => user.email, :subject => "Registered")
-   end
+  def registration_confirmation(user)
+    #@user = user
+    mail(:to => user.email, :subject => "Registered")
+  end
+
+  def email_subscribe(email)
+    mail(:to => email, :subject => "Registered")
+  end
 end
