f28012381a91cc8cdb224e89969610be4bcbc1b7
Elinor <elinor.dreamer@gmail.com>
2012-04-23 17:36:03 +0300

    Updating emails to be HTML - need CSS!

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,11 +2,11 @@ class UserMailer < ActionMailer::Base
   default :from => "subscribe@vtago.com"
 
   def registration_confirmation(user)
-    #@user = user
-    mail(:to => user.email, :subject => "Registered")
+    @user = user
+    mail(:to => user.email, :subject => "Welcome to VtagO", :content_type => "text/html")
   end
 
   def email_subscribe(email)
-    mail(:to => email, :subject => "Registered")
+    mail(:to => email, :subject => "Your invite request by VtagO", :content_type => "text/html")
   end
 end
