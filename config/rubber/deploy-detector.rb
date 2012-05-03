ef2e466931696fe8ab8ef27740eae3ea23b93476
kleinitay <itay@ubuntu.(none)>
2012-05-03 16:01:29 +0300

    fixed the deployment script

---
+++ config/rubber/deploy-detector.rb
@@ -0,0 +1,25 @@
+# config/rubber/deploy-delayed_job.rb
+
+namespace :rubber do
+  namespace :detector do
+
+    after "fix_and_compile:fix", "rubber:detector:restart"
+
+    desc "Stop the delayed_job process"
+    task :stop, :roles => :detector do
+      run "cd #{current_path}; RAILS_ENV=#{rails_env} script/delayed_job -n 5 --queues=detect stop"
+    end
+
+    desc "Start the delayed_job process"
+    task :start, :roles => :detector do
+      run "cd #{current_path}; RAILS_ENV=#{rails_env} script/delayed_job -n 5 --queues=detect start"
+    end
+
+    desc "Restart the delayed_job process"
+    task :restart, :roles => :detector do
+      run "cd #{current_path}; RAILS_ENV=#{rails_env} script/delayed_job -n 5 --queues=detect restart"
+    end
+
+
+  end
+end
