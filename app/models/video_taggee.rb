829e9953b81205043703020ed19315a030f0cda6
Elinor <elinor.dreamer@gmail.com>
2012-06-28 14:22:24 +0300

    fixing vtaggees page

--- app/models/video_taggee.rb
+++ app/models/video_taggee.rb
@@ -73,7 +73,7 @@ class VideoTaggee < ActiveRecord::Base
 
     def self.find_all_video_ids_by_user_id(user_fb_id)
       #VideoTaggee.find_all_by_fb_id(user_fb_id, :select => "video_id").map(&:video_id).uniq
-      VideoTaggee.find_by_sql("select video_id from videos, video_taggees where video_taggees.video_id = videos.id and videos.status_id != 0 group by videos.id")
+      VideoTaggee.find_by_sql("select video_id from videos, video_taggees where video_taggees.video_id = videos.id and video_taggees.fb_id = #{user_fb_id} and videos.status_id != 0 group by videos.id").map(&:video_id)
     end
 
     def to_s
